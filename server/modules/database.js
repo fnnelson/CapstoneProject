f96bff531a93bce459bb2918c5301b830358318c
Alice Smith <alice@smith.com>
2024-02-09 10:41:16 -0500

    add txt file

--- server/modules/database.js
+++ server/modules/database.js
@@ -1,20 +1,20 @@
 // // database name: [DB name here]
 
-// const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb");
 
-// const url = "mongodb://127.0.0.1:27017/[database name here]";
-// const client = new MongoClient(url);
+const url = "mongodb://127.0.0.1:27017/pma";
+const client = new MongoClient(url);
 
-// /*
-// const dbName = "swapi";
-// const collectionName = "characters";
-// let collection;
-// */
+/*
+const dbName = "swapi";
+const collectionName = "characters";
+let collection;
+*/
 
-// async function connectToMongoDB() {
-//     await client.connect();
-// }
+async function connectToMongoDB() {
+    await client.connect();
+}
 
-// connectToMongoDB();
+connectToMongoDB();
 
-// module.exports = { client };
+module.exports = { client };
--- server/routes/finders.js
+++ server/routes/finders.js
@@ -1,8 +1,7 @@
 const { client } = require("../modules/database.js");
-const characters = client.db().collection("characters");
-const planets = client.db().collection("planets");
-const films = client.db().collection("films");
-const filmCharacters = client.db().collection("films-characters");
+const projects = client.db().collection("projects");
+const tasks = client.db().collection("tasks");
+const users = client.db().collection("users");
 
 /*==================== FILM - CHARACTERS ====================*/
 module.exports.getFilmCharacters = (req, res) => {
@@ -47,45 +46,20 @@ module.exports.getFilmCharacters = (req, res) => {
     });
 };
 
-/*==================== CHARACTERS ====================*/
-module.exports.getCharacters = (req, res) => {
-  let data = characters.find({}).toArray();
+/*==================== PROJECTS ====================*/
+module.exports.getAllProjects = (req, res) => {
+  let data = projects.find({}).toArray();
   data
     .then((data) => {
       res.status(200).send(data);
     })
     .catch((err) => {
-      res.status(501).send({ alert: "Error getting all characters" }, err);
+      res.status(501).send({ alert: "Error getting all projects" }, err);
     });
 };
 
-module.exports.getCharacter = (req, res) => {
-  let data = characters.findOne({ id: Number(req.params.id) });
-  data
-    .then((data) => {
-      res.status(200).send(data);
-    })
-    .catch((err) => {
-      res
-        .status(501)
-        .send({ alert: `Error getting character with id ${id}` }, err);
-    });
-};
-
-/*==================== PLANETS ====================*/
-module.exports.getPlanets = (req, res) => {
-  let data = planets.find({}).toArray();
-  data
-    .then((data) => {
-      res.status(200).send(data);
-    })
-    .catch((err) => {
-      res.status(501).send({ alert: "Error getting all planets" }, err);
-    });
-};
-
-module.exports.getPlanet = (req, res) => {
-  let data = planets.findOne({ id: Number(req.params.id) });
+module.exports.getOneProject = (req, res) => {
+  let data = projects.findOne({ id: Number(req.params.id) });
   data
     .then((data) => {
       res.status(200).send(data);
@@ -93,61 +67,31 @@ module.exports.getPlanet = (req, res) => {
     .catch((err) => {
       res
         .status(501)
-        .send({ alert: `Error getting Planet with id ${id}` }, err);
+        .send({ alert: `Error getting project with id ${id}` }, err);
     });
 };
 
-/*==================== FILMS ====================*/
-module.exports.getFilms = (req, res) => {
-  let data = films.find({}).toArray();
+/*==================== TASKS ====================*/
+module.exports.getAllTasks = (req, res) => {
+  let data = tasks.find({}).toArray();
   data
     .then((data) => {
       res.status(200).send(data);
     })
     .catch((err) => {
-      res.status(501).send({ alert: "Error getting all films" }, err);
+      res.status(501).send({ alert: "Error getting all tasks" }, err);
     });
 };
 
-module.exports.getFilm = (req, res) => {
-  let data = films.findOne({ id: Number(req.params.id) });
+module.exports.getOneTask = (req, res) => {
+  let data = tasks.findOne({ id: Number(req.params.id) });
   data
     .then((data) => {
       res.status(200).send(data);
     })
     .catch((err) => {
-      res.status(501).send({ alert: `Error getting film with id ${id}` }, err);
+      res
+        .status(501)
+        .send({ alert: `Error getting Task with id ${id}` }, err);
     });
 };
-
-// mongodb query to get all characters in a single film (in this case we're searching for film with id 5)
-
-// db['films-characters'
-// ].aggregate([
-//   {
-//   $match: {
-//       "film_id": 5
-//     }
-//   },
-//   {
-//   $lookup: {from: "films", localField: "film_id", foreignField: "id", as: "film_matches"
-//     },
-//   },
-//   {
-//   $lookup: {from: "characters", localField: "character_id", foreignField: "id", as: "character_matches"
-//     },
-//   },
-//   {
-//   $unwind: "$character_matches"
-//   },
-//   {
-//   $replaceRoot: {
-//     newRoot: "$character_matches"
-//     }
-//   },
-//   {
-//   $sort: {
-//     id: 1
-//     }
-//   }
-// ])
