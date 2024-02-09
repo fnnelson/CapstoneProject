02aeb28941b4e2b1192e794a5d687a084b2503a1
Alice Smith <alice@smith.com>
2024-02-09 16:56:03 -0500

    install Python-shell

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors")
+const { PythonShell } = require("python-shell");
 const app = express();
 const projectsRouter = require('./routes/projects.router')
 const tasksRouter = require('./routes/tasks.router')
@@ -12,6 +13,31 @@ app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/projects", projectsRouter);
 app.use("/api/tasks", tasksRouter);
 
+// Endpoint for ML Model Interference
+app.get("/predict", (req, res) => {
+  // Input data for prediction
+  const input_data = {
+    team_size: 4,
+    budget: 40000,
+    workload: 3
+  };
+
+  // Run Python script for inference
+  PythonShell.run(
+    "C:/CapstoneProject/load_model.py",
+    { args: [JSON.stringify(input_data)] },
+    (err, result) => {
+      if (err) {
+        console.error("Error:", err);
+        res.status(500).send("Error performing inference");
+      } else {
+        console.log("Prediction:", result);
+        res.status(200).json({ prediction: result });
+      }
+    }
+  );
+});
+
 /*==================== PORT ====================*/
 const port = 3000;
 console.log(
@@ -20,3 +46,4 @@ console.log(
 app.listen(port, () => {
     console.log(`(/◕ヮ◕)/ listening on port: ${port}`);
 });
+
